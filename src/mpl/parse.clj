c82de168347b7083dec838fb265178af87fccc02
Jason Carr <jac3559@rit.edu>
2016-03-05 19:56:26 -0500

    Initial parse.clj

---
+++ src/mpl/parse.clj
@@ -0,0 +1,15 @@
+(ns mpl.core
+  (:require [clojure.string :as str])
+  (:require [clojure.java.io :as io]))
+
+(defn runParser 
+  [filename]
+  (with-open [rdr (io/reader filename)] 
+     
+  ))
+
+(defn parse
+  []
+  )
+
+
