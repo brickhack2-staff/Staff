e81435ec37349bc0db5adc21816c10366625982b
Daniel Wysocki <dwysocki@oswego.edu>
2016-03-05 22:55:18 -0500

    Initial prototype for parser.

--- src/mpl/core.clj
+++ src/mpl/core.clj
@@ -1,6 +1,16 @@
 (ns mpl.core
+  (:require [mpl.parse :as parse]
+            [clojure.pprint :refer [pprint]])
   (:gen-class))
+
+; Placeholder for actual reading of source code from file.
+(def source
+"a:b
+c:d
+|a b c d|e f g h|")
+
 
 (defn -main
   [& args]
-  (println "Hello, World!"))
+  ; Parse the sample source code into internal representation and print.
+  (pprint (parse/parse-source source)))
--- src/mpl/parse.clj
+++ src/mpl/parse.clj
@@ -1,15 +1,76 @@
-(ns mpl.core
-  (:require [clojure.string :as str])
-  (:require [clojure.java.io :as io]))
+(ns mpl.parse
+  "Collection of functions which parse source code."
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]))
 
-(defn runParser 
-  [filename]
-  (with-open [rdr (io/reader filename)] 
-     
-  ))
+(defn split-header-body
+  "Splits the header and body in the given source code. The separation is
+  defined as the first | character.
 
-(defn parse
-  []
-  )
+  Inputs:
+      source-code: String
+
+  Outputs:
+      [header body]: [String String]"
+  [source-code]
+  (str/split source-code #"\|" 2))
+
+(defn parse-field
+  "Splits a field into a [key val] vector.
+
+  Inputs:
+      field: String
+
+  Outputs:
+      [key val]: [String String]"
+  [field]
+  (let [[key val] (str/split field #":" 2)]
+    [(keyword key)
+     ; Placeholder, in future parse as, e.g. int, depending on key.
+     val]))
+
+(defn parse-header
+  "Parses the header into a hash-map of key -> val pairs.
+
+  Inputs:
+      header: String
+
+  Outputs:
+      {key val, ...}: {Keyword Object, ...}"
+  [header]
+  (let [fields (str/split header #"\n")]
+    (into (hash-map) (map parse-field fields))))
 
 
+
+
+(defn parse-body
+  "Parses the body of the source code into some internal representation.
+
+  NOTE: Currently does nothing, returns the body unchanged.
+
+  Inputs:
+      body: String
+
+  Outputs:
+      body: String"
+  [body]
+  ; placeholder
+  body)
+
+
+(defn parse-source
+  "Parses the source code into a hash-map.
+
+  Inputs:
+      source-code: String
+
+  Outputs:
+      {:header header,
+       :body body}"
+  [source-code]
+  (let [[header body] (split-header-body source-code)
+        header (parse-header header)
+        body (parse-body body)]
+    {:header header,
+     :body body}))
